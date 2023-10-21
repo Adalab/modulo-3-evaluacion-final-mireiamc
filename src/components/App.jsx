36dcac17c12ecc27113d24e0d9f4a003e67cbebc
¨mireiamc¨ <mireia.martin.carmona@gmail.com>
2023-10-21 14:52:37 +0200

    setting localStorage, filters html structure, header structure and background

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,39 @@
-import React from 'react';
+// Fichero src/components/App.jsx
+import { useState, useEffect } from 'react';
+
 import '../styles/App.scss';
+
+import getDataFromAPI from '../services/api';
+import ls from '../services/localStorage';
+
 import MovieSceneList from './MovieSceneList';
-// import Filters from './Filters';
+import Filters from './Filters';
 // import MovieSceneItem from './MovieSceneItem';
 // import MovieSceneDetail from './MovieSceneDetail';
-import getDataFromAPI from '../services/api';
-// import ls from '../services/localStorage';
-
-// Fichero src/components/App.jsx
-import {useState, useEffect} from 'react';
 
 const App = () => {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState(ls.get('movies', []));
 
   useEffect(() => {
-    getDataFromAPI().then((cleanData) => {
-      setMovies(cleanData);
-    });
+    if (movies.length === 0) {
+      getDataFromAPI().then((cleanData) => {
+        setMovies(cleanData);
+        ls.set('movies', cleanData);
+      });
+    }
   }, []);
 
   return (
-    <main className='movies__container'>
-      <MovieSceneList movies={movies}/>
-    </main>
+    <div className='page__container'>
+      <header className='header'>
+        <h1 className='page__title'>Owen Wilson's "WoW"</h1>
+      </header>
+      <main className='movies__container'>
+        <Filters />
+        <MovieSceneList movies={movies} />
+      </main>
+      <footer></footer>
+    </div>
   );
 };
 
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,7 +1,36 @@
 import '../styles/layout/Filters.scss';
 
 const Filters = () => {
-    return (console.log('Soy Filters'))
+  return (
+    <>
+      <form className='form'>
+        <label className='form__movie' htmlFor='search_name'>
+          Movie:
+          <input
+            className='form__input--movie'
+            type='text'
+            name='search_name'
+            id='search_name'
+            //   value={nameFilter}
+            //   onChange={handleInput}
+          />
+        </label>
+        <label className='form__year' htmlFor='search_city'>
+          Year:
+          <select
+            className='form__input--year'
+            name='search_city'
+            id='search_city'
+            value=''
+            //   onChange={handleSelect}
+          >
+            <option value=''>All</option>
+            {/* {renderCountriesOptions()} */}
+          </select>
+        </label>
+      </form>
+    </>
+  );
 };
 
 export default Filters;
