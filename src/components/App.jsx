78d1d2a4560b50a7a4ffa9a32804e314b6ecb92b
¨mireiamc¨ <mireia.martin.carmona@gmail.com>
2023-10-22 17:56:27 +0200

    filters working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,6 +13,8 @@ import Filters from './Filters';
 
 const App = () => {
   const [movies, setMovies] = useState(ls.get('movies', []));
+  const [movieFilter, setMovieFilter] = useState('');
+  const [yearFilter, setYearFilter] = useState('');
 
   useEffect(() => {
     if (movies.length === 0) {
@@ -23,14 +25,44 @@ const App = () => {
     }
   }, []);
 
+  const handleChange = (value) => {
+    setMovieFilter(value);
+  };
+
+  const handleChangeYear = (value) => {
+    setYearFilter(value);
+  };
+
+  const filteredMovies = movies
+    .filter((movie) =>
+      movie.name.toLowerCase().includes(movieFilter.toLowerCase())
+    )
+    .filter((item) => {
+      console.log(yearFilter);
+      if (yearFilter === '') {
+        return true;
+      } else {
+        return parseInt(yearFilter) === item.year;
+      } //item.year.toString()??
+    });
+  // .sort((a, b) => a.name.localCompare(b.name));
+
+  const years = movies.map((movie) => movie.year);
+
   return (
     <div className='page__container'>
       <header className='header'>
         <h1 className='page__title'>Owen Wilson's "WoW"</h1>
       </header>
       <main className='movies__container'>
-        <Filters />
-        <MovieSceneList movies={movies} />
+        <Filters
+          movieFilter={movieFilter}
+          handleChange={handleChange}
+          yearFilter={yearFilter}
+          handleChangeYear={handleChangeYear}
+          years={years}
+        />
+        <MovieSceneList movies={filteredMovies} />
       </main>
       <footer></footer>
     </div>
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,6 +1,28 @@
 import '../styles/layout/Filters.scss';
 
-const Filters = () => {
+const Filters = ({
+  movieFilter,
+  handleChange,
+  yearFilter,
+  handleChangeYear,
+  years,
+}) => {
+  const handleInput = (ev) => {
+    handleChange(ev.target.value);
+  };
+
+  const handleSelectYear = (ev) => {
+    handleChangeYear(ev.target.value);
+  };
+
+  const renderYearsOptions = () => {
+    return years.map((year, index) => (
+      <option value={year} key={index}>
+        {year}
+      </option>
+    ));
+  };
+
   return (
     <>
       <form className='form'>
@@ -9,23 +31,23 @@ const Filters = () => {
           <input
             className='form__input--movie'
             type='text'
-            name='search_name'
-            id='search_name'
-            //   value={nameFilter}
-            //   onChange={handleInput}
+            name='search_movie'
+            id='search_movie'
+            value={movieFilter}
+            onChange={handleInput}
           />
         </label>
         <label className='form__year' htmlFor='search_city'>
           Year:
           <select
             className='form__input--year'
-            name='search_city'
-            id='search_city'
-            value=''
-            //   onChange={handleSelect}
+            name='search_year'
+            id='search_year'
+            value={yearFilter}
+            onChange={handleSelectYear}
           >
             <option value=''>All</option>
-            {/* {renderCountriesOptions()} */}
+            {renderYearsOptions()}
           </select>
         </label>
       </form>
