0741863059c9540df2c7c3ebcc51d9f3217d9313
¨mireiamc¨ <mireia.martin.carmona@gmail.com>
2023-10-24 00:08:10 +0200

    solving some style problems

--- src/components/MovieSceneList.jsx
+++ src/components/MovieSceneList.jsx
@@ -24,7 +24,7 @@ const MovieSceneList = ({ movies, movieFilter }) => {
 
   return (
     <>
-      <ul className='movies'>{renderMoviesList}</ul>
+      <ul className='movies__list'>{renderMoviesList}</ul>
     </>
   );
 };
